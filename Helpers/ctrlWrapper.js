036c6f7e0a176f6dc2210dcddffb060fbaae6080
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-28 22:05:15 +0300

    ctrlW-module-auth-diskon-path-to

--- Helpers/ctrlWrapper.js
+++ Helpers/ctrlWrapper.js
@@ -1,13 +1,13 @@
-function ctrlWrapper(controller) {
+const ctrlWrapper = (controller) => {
   return async (req, res, next) => {
     try {
-      console.log("ctrlWrapper");
+      console.log("ctrlWrapper")
       await controller(req, res, next);
     } catch (error) {
       next(error);
     }
   };
-}
+};
 
 
 module.exports = ctrlWrapper
