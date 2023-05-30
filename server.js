7dcd1091b10a797b80dd7bb9d5024b3642523609
IvanPanin675 <jovanni.94.van@gmail.com>
2023-05-30 17:35:11 +0300

    server

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// зберігається в env
-// DB_HOST=mongodb+srv://ivanpanin:N9qUAWfq3gBavJnq@cluster0.amkeg2b.mongodb.net/food-deliver?retryWrites=true&w=majority
-
 const { DB_HOST } = process.env;
 
 mongoose.set("strictQuery", true);
